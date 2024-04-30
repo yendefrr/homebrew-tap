fa188a16ced5318607d26c04893405869c6a3c99
yendefrr <github@yendefr.xyz>
2024-04-30 15:08:35 +0300

    checksum

--- Formula/sql-alerts.rb
+++ Formula/sql-alerts.rb
@@ -3,7 +3,7 @@ class SqlAlerts < Formula
   desc "Get notifications about new rows in your database"
   homepage "https://github.com/yendefrr/sql-alerts"
   url "https://github.com/yendefrr/sql-alerts/archive/v0.4.3.tar.gz"
-  sha256 "sha256-of-the-tarball"
+  sha256 "a6aba0e169fb495ab8f1f8d01f85bbd8f20284ab9f1d1f3adc1e9db354f0acdf"
 
   depends_on "go" => :build
 
