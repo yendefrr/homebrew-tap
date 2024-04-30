bab5e1cd6840859380ab787ceec0040846d06002
yendefrr <github@yendefr.xyz>
2024-04-30 15:44:53 +0300

    sql-alerts

--- Formula/sql-alerts.rb
+++ Formula/sql-alerts.rb
@@ -1,8 +1,9 @@
-
 class SqlAlerts < Formula
   desc "Get notifications about new rows in your database"
   homepage "https://github.com/yendefrr/sql-alerts"
-  url "https://github.com/yendefrr/sql-alerts/archive/v0.4.7.tar.gz"
+  url "https://github.com/yendefrr/sql-alerts.git",
+    tag: "v0.4.7",
+    revision: "0d33c5b7d48f8ed94d4b7c91d660c16562250716"
   sha256 "430e2c8a1a5da29c0f2543e5047ccc5e069214d43bdd9b2b8364af71be0f8aea"
 
   depends_on "go" => :build
