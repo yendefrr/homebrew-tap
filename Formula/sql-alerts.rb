8fa88bacd106ea782dde5505aa72650b112ae22b
yendefrr <github@yendefr.xyz>
2024-04-30 15:05:45 +0300

    init

---
+++ Formula/sql-alerts.rb
@@ -0,0 +1,19 @@
+
+class SqlAlerts < Formula
+  desc "Get notifications about new rows in your database"
+  homepage "https://github.com/yendefrr/sql-alerts"
+  url "https://github.com/yendefrr/sql-alerts/archive/v0.4.3.tar.gz"
+  sha256 "sha256-of-the-tarball"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    # Add test code here if applicable
+    system "#{bin}/sql-alerts", "--version"
+  end
+end
